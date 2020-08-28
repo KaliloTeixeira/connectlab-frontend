74d0a55b8821e0da06c2f0cbaa03209f536f7887
KaliloTeixeira <kalilo555@hotmail.com>
2020-08-28 15:03:47 -0300

    feat/ Configurate paginate.

--- src/pages/student/index.js
+++ src/pages/student/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import api from '../../services/api';
 
+import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from 'react-icons/md';
 import './styles.css';
 
+import { Link } from 'react-router-dom';
 import Header from '../../components/Header';
 import LabComponent from './lab-component/';
 
@@ -12,7 +14,9 @@ export default class Student extends React.Component {
 
     state = {
         labs: [],
-        schools: []
+        labsInfo: [],
+        schools: [],
+        page: 1,
     }
 
     async componentDidMount() {
@@ -22,16 +26,38 @@ export default class Student extends React.Component {
     async getSchools() {
         const response = await api.get('/');
         this.setState({ schools: response.data });
+
         this.getLabs();
     }
 
-    async getLabs() {
-        const response = await api.get('/labs');
-        this.setState({ labs: response.data });
+    async getLabs(page = 1) {
+        const response = await api.get(`/labs?page=${page}`);
+        const { docs, ...labsInfo } = response.data;
+        this.setState({ labs: docs, labsInfo, page });
+    }
+
+    prevPage = () => {
+        const { page } = this.state;
+
+        if (page === 1) return;
+
+        const pageNumber = page - 1;
+        this.getLabs(pageNumber);
+        window.scrollTo(0, 0);
+    }
+
+    nextPage = () => {
+        const { page, labsInfo } = this.state;
+
+        if (page === labsInfo.pages) return;
+
+        const pageNumber = page + 1;
+        this.getLabs(pageNumber);
+        window.scrollTo(0, 0);
     }
 
     render() {
-        const { labs, schools } = this.state;
+        const { labs, schools, labsInfo, page } = this.state;
 
         return (
             <div className="student-page" >
@@ -40,16 +66,19 @@ export default class Student extends React.Component {
                     <h2>Laboratórios Cadastrados...</h2>
                 </div>
                 <div className="labs-container">
-
                     {labs.map(lab => {
                         const school = schools.find(school => school._id === lab.school);
                         return (
                             <LabComponent lab={lab} school={school} />
                         )
                     })}
-
-
                 </div>
+                <div className="page-actions" >
+                    <button disabled={page === 1} onClick={this.prevPage} > <MdKeyboardArrowLeft /> </button>
+                    <strong> {page} </strong>
+                    <button disabled={page === labsInfo.pages} onClick={this.nextPage} >  <MdKeyboardArrowRight /> </button>
+                </div>
+
             </div>
         )
     }
