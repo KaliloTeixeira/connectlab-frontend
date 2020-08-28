b9792cf2dccef7509dfb4b2e62affcbcac6df67d
KaliloTeixeira <kalilo555@hotmail.com>
2020-08-28 10:20:02 -0300

    Make dynamic School Title and School City at students page.

--- src/pages/student/index.js
+++ src/pages/student/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import api from '../../services/api';
 
 import './styles.css';
 
@@ -8,7 +9,30 @@ import LabComponent from './lab-component/';
 const title = "Encontre aqui um cantinho para estudar...";
 
 export default class Student extends React.Component {
+
+    state = {
+        labs: [],
+        schools: []
+    }
+
+    async componentDidMount() {
+        this.getSchools();
+    }
+
+    async getSchools() {
+        const response = await api.get('/');
+        this.setState({ schools: response.data });
+        this.getLabs();
+    }
+
+    async getLabs() {
+        const response = await api.get('/labs');
+        this.setState({ labs: response.data });
+    }
+
     render() {
+        const { labs, schools } = this.state;
+
         return (
             <div className="student-page" >
                 <Header title={title} />
@@ -16,9 +40,15 @@ export default class Student extends React.Component {
                     <h2>Laboratórios Cadastrados...</h2>
                 </div>
                 <div className="labs-container">
-                    <LabComponent />
-                    <LabComponent />
-                    <LabComponent />
+
+                    {labs.map(lab => {
+                        const school = schools.find(school => school._id === lab.school);
+                        return (
+                            <LabComponent lab={lab} school={school} />
+                        )
+                    })}
+
+
                 </div>
             </div>
         )
--- src/pages/student/lab-component/index.js
+++ src/pages/student/lab-component/index.js
@@ -3,22 +3,26 @@ import { Link } from 'react-router-dom';
 
 import './styles.css';
 
-const LabComponent = () => {
+const LabComponent = (props) => {
+
+    const school = props.school;
+    const lab = props.lab;
 
     return (
+
         <div className="lab-component">
             <div className="lab-component-header">
                 <div className="school-img-container" >
-                    <img className="school-img" src="https://media.glassdoor.com/sqll/2738518/rede-ftc-squarelogo-1564561646425.png" />
+                    <img className="school-img" src={school.img_url} />
                 </div>
                 <div className="title-container">
-                    <h1 className="school-name">FTC</h1>
-                    <h2 className="school-city">Vitória da Conquista - BA</h2>
+                    <h1 className="school-name">{school.name}</h1>
+                    <h2 className="school-city">{school.city}-{school.state}</h2>
                 </div>
             </div>
 
             <div className="contact-btn-container" >
-                <Link to="/contact" className="contact-btn" >Entrar em Contato</Link>
+                <Link to={`/contact/${school._id}`} className="contact-btn">Entrar em Contato</Link>
             </div>
 
         </div >
